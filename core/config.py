69fa109469e34d8c49165d494220f2e4bc150d67
acavalcanti <alexandrecavalcanti@gmail.com>
2026-04-18 15:13:38 -0300

    Correcao na leitura e estrutura do config.yaml

--- core/config.py
+++ core/config.py
@@ -4,10 +4,9 @@ def load_yaml(path):
     with open(path) as f:
         return yaml.safe_load(f)
 
-CONFIG = {
-    "main": load_yaml("config/config.yaml"),
-    "signatures": load_yaml("config/signatures.yaml")["signatures"],
-    "tools": load_yaml("config/tools.yaml")["tools"],
-    "kb": load_yaml("config/kb.yaml")["kb"],
-    "health": load_yaml("config/health.yaml")["health"]
-}
+CONFIG = load_yaml("config/config.yaml")
+
+CONFIG["signatures"] = load_yaml("config/signatures.yaml")["signatures"]
+CONFIG["tools"] = load_yaml("config/tools.yaml")["tools"]
+CONFIG["kb"] = load_yaml("config/kb.yaml")["kb"]
+CONFIG["health"] = load_yaml("config/health.yaml")["health"]
